45e5ebb7e4290aa0d2699f5e7e0e4a1a649e5fb3
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2015-10-21 09:06:07 -0500

    change format kalmanfilter

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -1,29 +1,33 @@
 import numpy as np
 
-def kalmanfilter(dataset, m0, C0, F, G, V, W):
+def kalmanfilter(dataset, m0, C0, Ft, Gt, V, W):
     """ Compute Kalman filter on dataset 
     starting with initial distribution N(m0, C0).
     Inputs:
-        dataset = np.array containing data -- (observations) x (time steps)
+        dataset = np.array containing data -- (time steps) x (observations)
         m0 = mean of initial state
         C0 = covariance matrix of initial state
         F, G = matrices defining the DLM
         V, W = covariance matrices for observation and model
     Outputs:
-        m_all = all successive means for state
-        C_all = all successive covariance matrices at each time step
-        a_all = all successive means for state predictive 
-        R_all = all successive covariance matrices at each time step """
-    m_all, a_all, C_all, R_all = [], [], [], []
-    nbobs, timesteps = dataset.shape
-    m = m0
+        m_all = means of state estimate (theta_t | y_{1:t})
+        C_all = covariance matrices of state estimate
+        a_all = means for state predictive (theta_t | y_{1:t-1})
+        R_all = covariance matrices for state predictive """
+    timesteps = len(dataset)
+    param = m0.size
+    m = m0.reshape((param, 1))
     C = C0
-    for ii in range(timesteps):
-        Y = dataset[:,ii]
+    m_all = np.zeros((timesteps, param))
+    a_all = np.zeros((timesteps, param))
+    C_all, R_all = [], []
+    ii = 0
+    for YT, F, G in zip(dataset, Ft, Gt):
+        Y = YT.T
         # State predictive
         a = G.dot(m)
         R = W + G.dot(C).dot(G.T)
-        a_all.append(a)
+        a_all[ii,:] = a.T
         R_all.append(R)
         # Intermediate step
         e = Y - F.dot(a)
@@ -33,6 +37,7 @@ def kalmanfilter(dataset, m0, C0, F, G, V, W):
         # State estimate
         m = a + KGain.dot(e)
         C = R - KGain.dot(F).dot(R)
-        m_all.append(m)
+        m_all[ii,:] = m.T
         C_all.append(C)
+        ii += 1
     return m_all, C_all, a_all, R_all
